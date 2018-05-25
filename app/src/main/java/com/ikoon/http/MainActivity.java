64c5cd30128231f844864f1640e3a37cda7ca0ff
MrKong <kongzheng1397@Foxmail.com>
2018-05-25 16:46:29 +0800

    添加demo工程样例

---
+++ app/src/main/java/com/ikoon/http/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ikoon.http;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ikoon.httplibrary.base.BaseApi;
+
+/**
+ * @author MrKong
+ */
+public class MainActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    
+    
+    }
+}
